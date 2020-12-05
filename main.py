034fdd42b8e028259ee72ee947fdea7cab6c2264
AverseABFun <arthurebeck@gmail.com>
2020-12-05 13:53:23 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+
+import os
+
+os.system('pip install --upgrade pip')
+os.system('pip install wheel')
+os.system('pip install twine')
+os.system('python charprint/setup.py sdist bdist_wheel')
+os.system('twine upload dist/*')
